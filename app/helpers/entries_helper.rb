5275c8feb35151b99ea5a59609c4c8562ff586ff
Patrik Jira <patrikjira@gmail.com>
2014-09-13 09:34:54 +0200

    Feat: revert colors on buttons to make white 'on' and gray 'off'

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -2,7 +2,7 @@ module EntriesHelper
 
   def link_to_entries(text, kind, current_kind, options = {})
     classes = options.delete(:class) || ""
-    classes << ' active' if current_kind == kind
+    classes << (current_kind == kind ? ' active' : ' inactive')
 
     link_to text, params.merge(kind: kind), options.merge(class: classes)
   end
