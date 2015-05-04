f375e262d6fba57d028ad55928ff6f74f0a54ac3
Patrik Jira <patrikjira@gmail.com>
2015-05-04 20:03:21 +0200

    fix: remove unsupported option

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -2,7 +2,6 @@ PDFKit.configure do |config|
 
   config.default_options = {
     :encoding      => 'UTF-8',
-    :page_size     => 'Letter',
     :margin_top    => '0.2in',
     :margin_right  => '0.2in',
     :margin_bottom => '0.4in',
